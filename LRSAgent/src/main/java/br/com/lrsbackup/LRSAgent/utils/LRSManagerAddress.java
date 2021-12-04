3b88122ab00c59eff4856a8e3002011571746a04
Cesar Bianchi <cesar_bianchi@hotmail.com>
2021-12-03 22:38:01 -0300

    Almost Ready!

--- LRSAgent/src/main/java/br/com/lrsbackup/LRSAgent/utils/LRSManagerAddress.java
+++ LRSAgent/src/main/java/br/com/lrsbackup/LRSAgent/utils/LRSManagerAddress.java
@@ -10,7 +10,13 @@ public class LRSManagerAddress {
 	}
 
 	private void setLRSManagerURI() {
-		this.LRSManagerURI = "http://192.168.0.101:6001/LRSManager";
+		boolean inEclipse = !(System.getenv("eclipse42") == null);
+		
+		if (inEclipse) {
+			this.LRSManagerURI = "http://127.0.0.1:8080/LRSManager";
+		} else {
+			this.LRSManagerURI = "http://192.168.0.101:6001/LRSManager"; //TODO - Not HARDCODED
+		}
 		
 	}
 
